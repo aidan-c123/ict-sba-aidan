8e082438a027fd110fc442a3d2c59ad9877af6b4
unknown <aidanchoi09@gmail.com>
2026-08-03 17:43:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Club Financial Report")
+with st.form("Login"):
+    role = st.selectbox("What is your role?", ("Club Financial Secretary", "Student's Union"))
+    if role == "Club Financial Secretary":
+        club = st.selectbox("What is your club?", ("Music Society", "Civic Club"))
+    st.form_submit_button('Submit')
+
