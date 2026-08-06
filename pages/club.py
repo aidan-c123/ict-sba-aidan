794c86a42aa8d35a1ed7c8e7f45aa66fdd51c748
unknown <aidanchoi09@gmail.com>
2026-08-06 15:11:12 +0800

    fixed installl

--- pages/club.py
+++ pages/club.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 f = open("id.txt", "r")
 info = f.readlines()
-st.title(f"{info[1].strip()} Financial Dashboard")
+st.title(f"{info[0].strip()} Financial Dashboard")
 
 back = st.button("Exit to Login")
 
