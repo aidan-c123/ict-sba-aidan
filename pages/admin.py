f65e97eb502d93d0d0865f33ad6dce379de53559
aidanc_ <aidanchoi09@gmail.com>
2026-09-14 09:07:41 +0800

    put add del update tabs under "edit records"

--- pages/admin.py
+++ pages/admin.py
@@ -12,7 +12,7 @@ login.close()
 
 st.title("Students' Union Financial Dashboard")
 
-total_summary, club_summary, view, add, update, delete = st.tabs(["View Master Summary", "View Club Summary", "View Records", "Add Record", "Update record", "Delete Record"])
+total_summary, club_summary, view, edit_records, edit_users = st.tabs(["View Master Summary", "View Club Summary", "View Records", "Edit Records", "Edit Users"])
 
 df = pd.read_csv('records.csv', index_col=False).sort_values(by="Date", ascending=True)
 copy = df.copy()
@@ -92,131 +92,133 @@ with view:
         }
     )
 
-with add:
-    with st.form("inputRecord", clear_on_submit=True):
-        club = st.selectbox("Club:", list(data.keys()), index=None, placeholder="Select club...")
-        name = st.text_input("Item Name:")
-        category = st.selectbox("Item Category", list(cat["Name"]), placeholder="Select category...", index=None)
-        expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
-        amount = st.number_input("Amount ($):", min_value=0.0)
-        date = str(st.date_input("Date:"))
-        add_submit = st.form_submit_button("Submit")
+with edit_records:
+    add, update, delete = st.tabs(["Add Record", "Update Record", "Delete Record"])
+    with add:
+        with st.form("inputRecord", clear_on_submit=True):
+            club = st.selectbox("Club:", list(data.keys()), index=None, placeholder="Select club...")
+            name = st.text_input("Item Name:")
+            category = st.selectbox("Item Category", list(cat["Name"]), placeholder="Select category...", index=None)
+            expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
+            amount = st.number_input("Amount ($):", min_value=0.0)
+            date = str(st.date_input("Date:"))
+            add_submit = st.form_submit_button("Submit")
 
-if add_submit:
-    with open('records.csv', 'a', newline="") as file:
+    if add_submit:
+        with open('records.csv', 'a', newline="") as file:
 
-        writer = csv.writer(file)
-        record_id = str(uuid.uuid4())
-        writer.writerow([club, name, category, expense_type, amount, date, record_id])
+            writer = csv.writer(file)
+            record_id = str(uuid.uuid4())
+            writer.writerow([club, name, category, expense_type, amount, date, record_id])
 
-    st.success("Submitted!")
-    time.sleep(2)
-    st.rerun()
-
-
-
-with update:
-    if 'update_confirm' not in st.session_state:
-        st.session_state.update_confirm = False
-    if 'update_uuid' not in st.session_state:
-        st.session_state.update_uuid = ""
-
-    with st.form("update", clear_on_submit=True):
-        uuid = st.text_input("UUID of the record to update:")
-        update_submit = st.form_submit_button("Submit")
-
-        if update_submit and uuid:
-            st.session_state.update_uuid = uuid
-            st.session_state.update_confirm = True
-
-    if st.session_state.update_confirm and st.session_state.update_uuid:
-        st.write("This is the record that will be updated:")
-
-        if copy[copy["UUID"] == st.session_state.update_uuid].empty:
-            st.warning("No record found with that UUID")
-            update_clear = st.button("Clear")
-            if update_clear:
-                st.session_state.update_confirm = False
-                st.session_state.update_uuid = ""
-                st.rerun()
-        else:
-            st.dataframe(
-            copy[copy["UUID"] == st.session_state.update_uuid],
-            hide_index=True,
-            column_config={
-                "UUID": st.column_config.Column(width=25),
-                "Amount": st.column_config.NumberColumn("Amount", format="dollar")
-                }
-            )
-
-            with st.form("update_items", clear_on_submit=True):
-                st.write("Leave input blank if it is not needed to be updated:")
-                club = st.selectbox("Club:", list(data.keys()), index=None, placeholder="Select club...")
-                name = st.text_input("Item Name:")
-                category = st.selectbox("Item Category", list(cat["Name"]), placeholder="Select category...", index = None)
-                expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
-                amount = st.number_input("Amount ($):", min_value=0.0, value=None)
-                date = str(st.date_input("Date:", value = None))
-                update_confirm = st.form_submit_button("Confirm")
-
-            if update_confirm:
-                update_dict = {"Club" : club, "Item Name": name, "Category": category, "Type" : expense_type, "Amount": amount,"Date": date}
-                
-                for i in list(update_dict.keys()):
-                    if update_dict[i] is not None and update_dict[i] != "":
-                        df.loc[df["UUID"] == st.session_state.update_uuid, i] = update_dict[i]
-                        df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"] = abs(df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"])
-
-                df.to_csv("records.csv", index=False)
-                st.success("Record updated successfully!")
-                time.sleep(2)
-                st.session_state.update_confirm = False
-                st.session_state.update_uuid = ""
-                st.rerun()
+        st.success("Submitted!")
+        time.sleep(2)
+        st.rerun()
 
 
-with delete:
-    if 'del_confirm' not in st.session_state:
-        st.session_state.del_confirm = False
-    if 'del_uuid' not in st.session_state:
-        st.session_state.del_uuid = ""
 
-    with st.form("deleteRecord", clear_on_submit=True):
-        uuid = st.text_input("UUID of the record to delete:")
-        del_submit = st.form_submit_button("Submit")
+    with update:
+        if 'update_confirm' not in st.session_state:
+            st.session_state.update_confirm = False
+        if 'update_uuid' not in st.session_state:
+            st.session_state.update_uuid = ""
 
-        if del_submit and uuid:
-            st.session_state.del_uuid = uuid
-            st.session_state.del_confirm = True
+        with st.form("update", clear_on_submit=True):
+            uuid = st.text_input("UUID of the record to update:")
+            update_submit = st.form_submit_button("Submit")
 
-    if st.session_state.del_confirm and st.session_state.del_uuid:
-        st.write("This is the record that will be deleted:")
+            if update_submit and uuid:
+                st.session_state.update_uuid = uuid
+                st.session_state.update_confirm = True
 
-        if df[df["UUID"] == st.session_state.del_uuid].empty:
-            st.warning("No record found with that UUID")
-            del_clear = st.button("Clear")
-            if del_clear:
-                st.session_state.del_confirm = False
-                st.session_state.del_uuid = ""
-                st.rerun()
-        else:
-            st.dataframe(
-            df[df["UUID"] == st.session_state.del_uuid],
-            hide_index=True,
-            column_config={
-                "UUID": st.column_config.Column(width=25),
-                "Amount": st.column_config.NumberColumn("Amount", format="dollar")
-                }
-            )
-            del_confirm = st.button("Confirm")
-            if del_confirm:
-                df = df[df["UUID"] != st.session_state.del_uuid]
-                df.to_csv("records.csv", index=False)
-                st.success("Record deleted successfully!")
-                time.sleep(2)
-                st.session_state.del_confirm = False
-                st.session_state.del_uuid = ""
-                st.rerun()
+        if st.session_state.update_confirm and st.session_state.update_uuid:
+            st.write("This is the record that will be updated:")
+
+            if copy[copy["UUID"] == st.session_state.update_uuid].empty:
+                st.warning("No record found with that UUID")
+                update_clear = st.button("Clear")
+                if update_clear:
+                    st.session_state.update_confirm = False
+                    st.session_state.update_uuid = ""
+                    st.rerun()
+            else:
+                st.dataframe(
+                copy[copy["UUID"] == st.session_state.update_uuid],
+                hide_index=True,
+                column_config={
+                    "UUID": st.column_config.Column(width=25),
+                    "Amount": st.column_config.NumberColumn("Amount", format="dollar")
+                    }
+                )
+
+                with st.form("update_items", clear_on_submit=True):
+                    st.write("Leave input blank if it is not needed to be updated:")
+                    club = st.selectbox("Club:", list(data.keys()), index=None, placeholder="Select club...")
+                    name = st.text_input("Item Name:")
+                    category = st.selectbox("Item Category", list(cat["Name"]), placeholder="Select category...", index = None)
+                    expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
+                    amount = st.number_input("Amount ($):", min_value=0.0, value=None)
+                    date = str(st.date_input("Date:", value = None))
+                    update_confirm = st.form_submit_button("Confirm")
+
+                if update_confirm:
+                    update_dict = {"Club" : club, "Item Name": name, "Category": category, "Type" : expense_type, "Amount": amount,"Date": date}
+                    
+                    for i in list(update_dict.keys()):
+                        if update_dict[i] is not None and update_dict[i] != "":
+                            df.loc[df["UUID"] == st.session_state.update_uuid, i] = update_dict[i]
+                            df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"] = abs(df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"])
+
+                    df.to_csv("records.csv", index=False)
+                    st.success("Record updated successfully!")
+                    time.sleep(2)
+                    st.session_state.update_confirm = False
+                    st.session_state.update_uuid = ""
+                    st.rerun()
+
+
+    with delete:
+        if 'del_confirm' not in st.session_state:
+            st.session_state.del_confirm = False
+        if 'del_uuid' not in st.session_state:
+            st.session_state.del_uuid = ""
+
+        with st.form("deleteRecord", clear_on_submit=True):
+            uuid = st.text_input("UUID of the record to delete:")
+            del_submit = st.form_submit_button("Submit")
+
+            if del_submit and uuid:
+                st.session_state.del_uuid = uuid
+                st.session_state.del_confirm = True
+
+        if st.session_state.del_confirm and st.session_state.del_uuid:
+            st.write("This is the record that will be deleted:")
+
+            if df[df["UUID"] == st.session_state.del_uuid].empty:
+                st.warning("No record found with that UUID")
+                del_clear = st.button("Clear")
+                if del_clear:
+                    st.session_state.del_confirm = False
+                    st.session_state.del_uuid = ""
+                    st.rerun()
+            else:
+                st.dataframe(
+                df[df["UUID"] == st.session_state.del_uuid],
+                hide_index=True,
+                column_config={
+                    "UUID": st.column_config.Column(width=25),
+                    "Amount": st.column_config.NumberColumn("Amount", format="dollar")
+                    }
+                )
+                del_confirm = st.button("Confirm")
+                if del_confirm:
+                    df = df[df["UUID"] != st.session_state.del_uuid]
+                    df.to_csv("records.csv", index=False)
+                    st.success("Record deleted successfully!")
+                    time.sleep(2)
+                    st.session_state.del_confirm = False
+                    st.session_state.del_uuid = ""
+                    st.rerun()
         
 
 with st.bottom:
--- pages/club.py
+++ pages/club.py
@@ -13,7 +13,7 @@ club = info[0].strip()
 
 st.title(f"{club} Financial Dashboard")
 
-summary, view, add, update, delete = st.tabs(["View Summary", "View Records", "Add Record", "Update record", "Delete Record"])
+summary, view, edit= st.tabs(["View Summary", "View Records", "Edit Records"])
 
 df = pd.read_csv('records.csv', index_col=False).sort_values(by="Date", ascending=True)
 copy = df.copy()[df["Club"] == club]
@@ -60,129 +60,130 @@ with view:
         }
     )
 
-with add:
-    with st.form("inputRecord", clear_on_submit=True):
-        name = st.text_input("Item Name:")
-        category = st.selectbox("Item Category", list(cat["Name"]), placeholder="Select category...", index=None)
-        expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
-        amount = st.number_input("Amount ($):", min_value=0.0)
-        date = st.date_input("Date:")
-        add_submit = st.form_submit_button("Submit")
+with edit:
+    add, update, delete = st.tabs(["Add Record", "Update Record", "Delete Record"])
+    with add:
+        with st.form("inputRecord", clear_on_submit=True):
+            name = st.text_input("Item Name:")
+            category = st.selectbox("Item Category", list(cat["Name"]), placeholder="Select category...", index=None)
+            expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
+            amount = st.number_input("Amount ($):", min_value=0.0)
+            date = st.date_input("Date:")
+            add_submit = st.form_submit_button("Submit")
 
-if add_submit:
-    with open('records.csv', 'a', newline="") as file:
+    if add_submit:
+        with open('records.csv', 'a', newline="") as file:
+            writer = csv.writer(file)
+            record_id = str(uuid.uuid4())
+            writer.writerow([record_id, club, name, category, expense_type, amount, date])
 
-        writer = csv.writer(file)
-        record_id = str(uuid.uuid4())
-        writer.writerow([record_id, club, name, category, expense_type, amount, date])
-
-    st.success("Submitted!")
-    time.sleep(2)
-    st.rerun()
-
-
-
-with update:
-    if 'update_confirm' not in st.session_state:
-        st.session_state.update_confirm = False
-    if 'update_uuid' not in st.session_state:
-        st.session_state.update_uuid = ""
-
-    with st.form("update", clear_on_submit=True):
-        uuid = st.text_input("UUID of the record to update:")
-        update_submit = st.form_submit_button("Submit")
-
-        if update_submit and uuid:
-            st.session_state.update_uuid = uuid
-            st.session_state.update_confirm = True
-
-    if st.session_state.update_confirm and st.session_state.update_uuid:
-        st.write("This is the record that will be updated:")
-
-        if copy[copy["UUID"] == st.session_state.update_uuid].empty:
-            st.warning("No record found with that UUID")
-            update_clear = st.button("Clear")
-            if update_clear:
-                st.session_state.update_confirm = False
-                st.session_state.update_uuid = ""
-                st.rerun()
-        else:
-            st.dataframe(
-            copy[copy["UUID"] == st.session_state.update_uuid],
-            hide_index=True,
-            column_config={
-                "UUID": st.column_config.Column(width=25),
-                "Amount": st.column_config.NumberColumn("Amount", format="dollar")
-                }
-            )
-
-            with st.form("update_items", clear_on_submit=True):
-                st.write("Leave input blank if it is not needed to be updated:")
-                name = st.text_input("Item Name:")
-                category = st.selectbox("Item Category", list(cat["Name"]))
-                expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
-                amount = st.number_input("Amount ($):", min_value=0.0, value=None)
-                date = st.date_input("Date:", value = None)
-                update_confirm = st.form_submit_button("Confirm")
-
-            if update_confirm:
-                update_dict = {"Club" : club, "Item Name": name,  "Category": category, "Type" : expense_type, "Amount": amount,"Date": date}
-                
-                for i in list(update_dict.keys()):
-                    if update_dict[i] is not None and update_dict[i] != "":
-                        df.loc[df["UUID"] == st.session_state.update_uuid, i] = update_dict[i]
-                        df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"] = abs(df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"])
-
-                df.to_csv("records.csv", index=False)
-                st.success("Record updated successfully!")
-                time.sleep(2)
-                st.session_state.update_confirm = False
-                st.session_state.update_uuid = ""
-                st.rerun()
+        st.success("Submitted!")
+        time.sleep(2)
+        st.rerun()
 
 
-with delete:
-    if 'del_confirm' not in st.session_state:
-        st.session_state.del_confirm = False
-    if 'del_uuid' not in st.session_state:
-        st.session_state.del_uuid = ""
 
-    with st.form("deleteRecord", clear_on_submit=True):
-        uuid = st.text_input("UUID of the record to delete:")
-        del_submit = st.form_submit_button("Submit")
+    with update:
+        if 'update_confirm' not in st.session_state:
+            st.session_state.update_confirm = False
+        if 'update_uuid' not in st.session_state:
+            st.session_state.update_uuid = ""
 
-        if del_submit and uuid:
-            st.session_state.del_uuid = uuid
-            st.session_state.del_confirm = True
+        with st.form("update", clear_on_submit=True):
+            uuid = st.text_input("UUID of the record to update:")
+            update_submit = st.form_submit_button("Submit")
 
-    if st.session_state.del_confirm and st.session_state.del_uuid:
-        st.write("This is the record that will be deleted:")
+            if update_submit and uuid:
+                st.session_state.update_uuid = uuid
+                st.session_state.update_confirm = True
 
-        if copy[copy["UUID"] == st.session_state.del_uuid].empty:
-            st.warning("No record found with that UUID")
-            del_clear = st.button("Clear")
-            if del_clear:
-                st.session_state.del_confirm = False
-                st.session_state.del_uuid = ""
-                st.rerun()
-        else:
-            st.dataframe(
-            copy[copy["UUID"] == st.session_state.del_uuid],
-            hide_index=True,
-            column_config={
-                "UUID": st.column_config.Column(width=25),
-                "Amount": st.column_config.NumberColumn("Amount", format="dollar")
-                }
-            )
-            del_confirm = st.button("Confirm")
-            if del_confirm:
-                df = df[df["UUID"] != st.session_state.del_uuid]
-                df.to_csv("records.csv", index=False)
-                st.success("Record deleted successfully!")
-                time.sleep(2)
-                st.session_state.del_confirm = False
-                st.session_state.del_uuid = ""
-                st.rerun()
+        if st.session_state.update_confirm and st.session_state.update_uuid:
+            st.write("This is the record that will be updated:")
+
+            if copy[copy["UUID"] == st.session_state.update_uuid].empty:
+                st.warning("No record found with that UUID")
+                update_clear = st.button("Clear")
+                if update_clear:
+                    st.session_state.update_confirm = False
+                    st.session_state.update_uuid = ""
+                    st.rerun()
+            else:
+                st.dataframe(
+                copy[copy["UUID"] == st.session_state.update_uuid],
+                hide_index=True,
+                column_config={
+                    "UUID": st.column_config.Column(width=25),
+                    "Amount": st.column_config.NumberColumn("Amount", format="dollar")
+                    }
+                )
+
+                with st.form("update_items", clear_on_submit=True):
+                    st.write("Leave input blank if it is not needed to be updated:")
+                    name = st.text_input("Item Name:")
+                    category = st.selectbox("Item Category", list(cat["Name"]))
+                    expense_type = st.radio("Is this an income or expense?", ["Income", "Expense"], index=None, horizontal=True)
+                    amount = st.number_input("Amount ($):", min_value=0.0, value=None)
+                    date = st.date_input("Date:", value = None)
+                    update_confirm = st.form_submit_button("Confirm")
+
+                if update_confirm:
+                    update_dict = {"Club" : club, "Item Name": name,  "Category": category, "Type" : expense_type, "Amount": amount,"Date": date}
+                    
+                    for i in list(update_dict.keys()):
+                        if update_dict[i] is not None and update_dict[i] != "":
+                            df.loc[df["UUID"] == st.session_state.update_uuid, i] = update_dict[i]
+                            df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"] = abs(df.loc[df["UUID"] == st.session_state.update_uuid, "Amount"])
+
+                    df.to_csv("records.csv", index=False)
+                    st.success("Record updated successfully!")
+                    time.sleep(2)
+                    st.session_state.update_confirm = False
+                    st.session_state.update_uuid = ""
+                    st.rerun()
+
+
+    with delete:
+        if 'del_confirm' not in st.session_state:
+            st.session_state.del_confirm = False
+        if 'del_uuid' not in st.session_state:
+            st.session_state.del_uuid = ""
+
+        with st.form("deleteRecord", clear_on_submit=True):
+            uuid = st.text_input("UUID of the record to delete:")
+            del_submit = st.form_submit_button("Submit")
+
+            if del_submit and uuid:
+                st.session_state.del_uuid = uuid
+                st.session_state.del_confirm = True
+
+        if st.session_state.del_confirm and st.session_state.del_uuid:
+            st.write("This is the record that will be deleted:")
+
+            if copy[copy["UUID"] == st.session_state.del_uuid].empty:
+                st.warning("No record found with that UUID")
+                del_clear = st.button("Clear")
+                if del_clear:
+                    st.session_state.del_confirm = False
+                    st.session_state.del_uuid = ""
+                    st.rerun()
+            else:
+                st.dataframe(
+                copy[copy["UUID"] == st.session_state.del_uuid],
+                hide_index=True,
+                column_config={
+                    "UUID": st.column_config.Column(width=25),
+                    "Amount": st.column_config.NumberColumn("Amount", format="dollar")
+                    }
+                )
+                del_confirm = st.button("Confirm")
+                if del_confirm:
+                    df = df[df["UUID"] != st.session_state.del_uuid]
+                    df.to_csv("records.csv", index=False)
+                    st.success("Record deleted successfully!")
+                    time.sleep(2)
+                    st.session_state.del_confirm = False
+                    st.session_state.del_uuid = ""
+                    st.rerun()
         
 
 with st.bottom:
